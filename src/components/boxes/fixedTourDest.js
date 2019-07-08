0d45b9e68d55bb7e8682be192103e6081b01930d
Navigate_17 <navigate_17@Navigate-17s-MacBook-Pro.local>
2019-07-08 16:41:13 +1200

    testing for tablet

--- src/components/boxes/fixedTourDest.js
+++ src/components/boxes/fixedTourDest.js
@@ -13,23 +13,23 @@ const FixedTourDest = ({
   hotText,
 }) => {
   return (
-    <div className="fixed-tour__container">
-      <figure>
-        <img src={imageData} alt={imageAlt} />
+    <div className="fixed-tour">
+      <figure className="fixed-tour__figure">
+        <img className="fixed-tour__image" src={imageData} alt={imageAlt} />
         <figcaption className="tour-banner__figure-caption tour-banner__figure-caption-newzealand">
           <span className="tour-banner__days">{days}</span>
         </figcaption>
       </figure>
-      <h2>{title}</h2>
-      <h5>{subTitle}</h5>
-      <div>
-        <span>{days}</span>
-        <span>{price}</span>
-        <span>{perDay}</span>
+      <h2 className="fixed-tour__title">{title}</h2>
+      <h5 className="fixed-tour__subtitle">{subTitle}</h5>
+      <div className="fixed-tour__info">
+        <span className="fixed-tour__days">{days}</span>
+        <span className="fixed-tour__price">{price}</span>
+        <span className="fixed-tour__per-day">{perDay}</span>
       </div>
-      <h5>{earlyBird}</h5>
-      <a>{availablity}</a>
-      <p>{hotText}</p>
+      <h5 className="fixed-tour__early-bird">{earlyBird}</h5>
+      <a className="fixed-tour__availablitity">{availablity}</a>
+      <p className="fixed-tour__hot-text">{hotText}</p>
     </div>
   )
 }
