664a89a5e1299e318e6ae1e25b2ce89d6a1adeee
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-07-26 11:58:17 +1200

    finalized FAQ alpha

--- packages/wildkiwi/src/components/sectionFAQ.js
+++ packages/wildkiwi/src/components/sectionFAQ.js
@@ -1,27 +1,83 @@
 import React, { useState } from "react"
 
-import usefaqQuery from "../queries/faqQuery"
+import useFAQQuery from "../queries/faqQuery"
 
 const SectionVehicles = () => {
   // extracting our custom hook
-  const FAQData = usefaqQuery()
-  console.log(FAQData)
+  const FAQData = useFAQQuery()
 
-  // setting our initial state
+  // function that handles the state change
+  const handleClick = idx => {
+    // setting the color
+    setActiveIndex(idx)
 
-  const { initialState, setInitial } = useState(FAQData)
+    // conditionally rendering FAQ categories
+    if (idx === 0) {
+      setCategory(categoryAData)
+    }
 
-  console.log(initialState)
+    if (idx === 1) {
+      setCategory(categoryBData)
+    }
+
+    if (idx === 2) {
+      setCategory(categoryCData)
+    }
+
+    if (idx === 3) {
+      setCategory(categoryDData)
+    }
+    // setColor("FAQ__button FAQ__button--active")
+  }
 
   // creating variables to store category strings
-  const categoryA = "ABOUT YOUR TRIP"
+  const categories = [
+    { label: "ABOUT YOUR TRIP" },
+    { label: "BUDGET & PAYMENT" },
+    { label: "TRANSPORT" },
+    { label: "TRAVEL & SAFTEY" },
+  ]
 
   // filter out what we need for CategoryA
-  const categoryAData = FAQData.filter(faq => faq.node.category === categoryA)
+  const categoryAData = FAQData.filter(
+    faq => faq.node.category === categories[0].label
+  )
+  const categoryBData = FAQData.filter(
+    faq => faq.node.category === categories[1].label
+  )
+  const categoryCData = FAQData.filter(
+    faq => faq.node.category === categories[2].label
+  )
+  const categoryDData = FAQData.filter(
+    faq => faq.node.category === categories[3].label
+  )
+
+  // setting our initial states
+  const [initialCategory, setCategory] = useState(categoryAData)
+  const [activeIndex, setActiveIndex] = useState(0)
+
+  // rendering buttons
+  const renderButtons = () => {
+    return categories.map((button, idx) => {
+      return (
+        <button
+          key={idx}
+          onClick={() => handleClick(idx)}
+          className={
+            idx === activeIndex
+              ? "FAQ__button FAQ__button--active"
+              : "FAQ__button"
+          }
+        >
+          {button.label}
+        </button>
+      )
+    })
+  }
 
   // using useState hook to set our inital state
   const renderFAQs = () => {
-    return categoryAData.map(element => {
+    return initialCategory.map(element => {
       return element.node.questions.map((questtion, idx) => {
         return (
           <li className="FAQ__list-item" key={idx}>
@@ -45,17 +101,58 @@ const SectionVehicles = () => {
     })
   }
 
+  // function to handle mobile's dropdown
+  const handleDropdown = e => {
+    if (e.target.value === categories[0].label) {
+      setCategory(categoryAData)
+    }
+
+    if (e.target.value === categories[1].label) {
+      setCategory(categoryBData)
+    }
+
+    if (e.target.value === categories[2].label) {
+      setCategory(categoryCData)
+    }
+
+    if (e.target.value === categories[3].label) {
+      setCategory(categoryDData)
+    }
+  }
+
+  // rendering video boxes
+  const renderVideoBoxes = () => {
+    return (
+      <div className="video-box">
+        <h3 className="video-boex-header">Title of video here</h3>
+        <div className="video-box" />
+      </div>
+    )
+  }
+
   return (
     <section className="section-FAQ">
       <div className="FAQ__container">
-        <div className="FAQ__buttons">
-          <button className="FAQ__button">ABOUT YOUR TRIP</button>
-          <button className="FAQ__button">BUDGET & PAYMENT</button>
-          <button className="FAQ__button">TRANSPORT</button>
-          <button className="FAQ__button">TRAVEL & SAFTEY</button>
+        <div className="FAQ__buttons">{renderButtons()}</div>
+        <div className="mobile-yes">
+          <div class="activity__selector">
+            <select
+              onChange={handleDropdown}
+              class="activity__dropdown"
+              id="country"
+            >
+              <option value="ABOUT YOUR TRIP">ABOUT YOUR TRIP</option>
+              <option value="BUDGET & PAYMENT">BUDGET & PAYMENT</option>
+              <option value="TRANSPORT">TRANSPORT</option>
+              <option value="TRAVEL & SAFTEY">TRAVEL & SAFTEY</option>
+            </select>
+          </div>
         </div>
         <div className="FAQ__accordian">
           <ul className="FAQ__list">{renderFAQs()}</ul>
+        </div>
+        <div className="FAQ__video-box">
+          {renderVideoBoxes()} {renderVideoBoxes()} {renderVideoBoxes()}
         </div>
       </div>
     </section>
