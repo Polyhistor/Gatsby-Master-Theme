ab4c3b2cba279c403566acb43777c03d6feba94f
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-09-05 17:26:02 +1200

    cleaned up shared modules, modularize some components

--- packages/shared/index.js
+++ packages/shared/index.js
@@ -1,69 +1,66 @@
+export {
+  default as ActivityRelated,
+} from "./components/activity/activityRelated"
+export {
+  default as ActivityScaffold,
+} from "./components/activity/activityScaffold"
+export {
+  default as SectionActivity,
+} from "./components/activity/sectionActivity"
+export { default as Banner } from "./components/banner/banner"
+export { default as TourBanner } from "./components/banner/tourBanner"
+export { default as GreenBarAlt } from "./components/bars/greenbar-alt"
+export { default as GreenBar } from "./components/bars/greenBar"
+export { default as NavLink } from "./components/blog/blogNavLink"
+export { default as BlogRelated } from "./components/blog/blogRelated"
 export { default as Box } from "./components/boxes/box"
-export { default as Landing } from "./components/header/landing"
-export { default as Layout } from "./components/layout/layout"
-export { default as MainCSS } from "./styles/main.scss"
+export { default as BoxContainer } from "./components/boxes/boxContainer"
+export {
+  default as ActivitiesBox,
+} from "./components/destinations/activitiesBox"
+export { default as Booking } from "./components/destinations/booking"
+export {
+  default as DestinationSection,
+} from "./components/destinations/destinationSection"
+export {
+  default as DestinationStarter,
+} from "./components/destinations/destinationStarter"
+export {
+  default as FilteredTour,
+} from "./components/destinations/filteredTours"
+export { default as GetThere } from "./components/destinations/getThere"
+export { default as HighLight } from "./components/destinations/highlight"
+export { default as Includes } from "./components/destinations/includes"
+export { default as Itinerary } from "./components/destinations/itinerary"
+export { default as TripBox } from "./components/destinations/tripBox"
+export { default as WhyWild } from "./components/destinations/whyWild"
 export { default as FooterCopyright } from "./components/footer/footerCopyright"
+export { default as FooterForm } from "./components/footer/footerForm"
 export {
   default as FooterInstagramBox,
 } from "./components/footer/footerInstagramBox"
-export { default as FooterForm } from "./components/footer/footerForm"
+export { default as Landing } from "./components/header/landing/landing"
+export {
+  default as LandingChartered,
+} from "./components/header/landings/landingChartered"
 export {
   default as DestinationsMobile,
 } from "./components/mobile/destinationsMobile"
-export {
-  default as DestinationsTablet,
-} from "./components/tablet/destinationsTablet"
-export { default as TourBanner } from "./components/banner/tourBanner"
-export { default as Banner } from "./components/banner/banner"
-export { default as Popup } from "./components/popup"
-export { default as GreenBar } from "./components/bars/greenBar"
-export { default as GreenBarAlt } from "./components/bars/greenbar-alt"
-export { default as BoxContainer } from "./components/boxes/boxContainer"
 export { default as WhyWildKiwi } from "./components/mobile/whyWildkiwi"
 export { default as Review } from "./components/reviews/review"
-export { default as Trips } from "./components/trips/trips"
+export { default as SectionFAQ } from "./components/sections/sectionFAQ"
+export {
+  default as SectionGetInTouch,
+} from "./components/sections/sectionGetInTouch"
 export {
   default as SectionHowItWorks,
 } from "./components/sections/sectionHowItWorks"
 export {
   default as SectionVehicle,
 } from "./components/sections/sectionVehicles"
-export { default as SectionFAQ } from "./components/sections/sectionFAQ"
 export {
-  default as SectionGetInTouch,
-} from "./components/sections/sectionGetInTouch"
-export {
-  default as LandingChartered,
-} from "./components/header/landings/landingChartered"
-export {
-  default as DestinationSection,
-} from "./components/destinations/destinationSection"
-export { default as TripBox } from "./components/destinations/tripBox"
-export {
-  default as DestinationStarter,
-} from "./components/destinations/destinationStarter"
-export { default as HighLight } from "./components/destinations/highlight"
-export { default as Itinerary } from "./components/destinations/itinerary"
-export { default as Includes } from "./components/destinations/includes"
-export {
-  default as ActivitiesBox,
-} from "./components/destinations/activitiesBox"
-export { default as WhyWild } from "./components/destinations/whyWild"
-export { default as Booking } from "./components/destinations/booking"
-export { default as GetThere } from "./components/destinations/getThere"
-
-export { default as FAQQuery } from "./queries/faqQuery"
-export {
-  default as FilteredTour,
-} from "./components/destinations/filteredTours"
-export { default as NavLink } from "./components/blog/blogNavLink"
-export {
-  default as SectionActivity,
-} from "./components/activity/sectionActivity"
-export {
-  default as ActivityScaffold,
-} from "./components/activity/activityScaffold"
-export {
-  default as ActivityRelated,
-} from "./components/activity/activityRelated"
-export { default as BlogRelated } from "./components/blog/blogRelated"
+  default as DestinationsTablet,
+} from "./components/tablet/destinationsTablet"
+export { default as Trips } from "./components/trips/trips"
+export { default as Popup } from "./components/popup"
+export { default as MainCSS } from "./styles/main.scss"
