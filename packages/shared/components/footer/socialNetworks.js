6733bbd987206fe440d4ea1a1a9a341284c844ea
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-09-12 16:36:27 +1200

    worked around rich  text renderer for activities, fixed a few styling issues, revamped map codes

--- packages/shared/components/footer/socialNetworks.js
+++ packages/shared/components/footer/socialNetworks.js
@@ -6,17 +6,29 @@ const SocialNetowrk = () => {
     <div className="u-margin-bottom-medium u-padding-top-huge ">
       <div className="footer__social-networks">
         <div className="footer__social-networks-box">
-          <a href="https://www.facebook.com/wildkiwitours" target="_blank">
+          <a
+            href="https://www.facebook.com/wildkiwitours"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <svg className="svg-icon--Facebook">
               <use xlinkHref={withPrefix("sprite.svg#icon-Facebook")} />
             </svg>
           </a>
-          <a href="https://www.instagram.com/wildkiwitours/" target="_blank">
+          <a
+            href="https://www.instagram.com/wildkiwitours/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <svg className="svg-icon--Instagram">
               <use xlinkHref={withPrefix("sprite.svg#icon-Instagram")} />
             </svg>
           </a>
-          <a href="https://www.youtube.com/c/WildkiwiTours" target="_blank">
+          <a
+            href="https://www.youtube.com/c/WildkiwiTours"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <svg className="svg-icon--Youtube">
               <use xlinkHref={withPrefix("sprite.svg#icon-Youtube")} />
             </svg>
