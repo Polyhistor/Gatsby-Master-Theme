26189e3a217b170ce5f7ab38716357c3d8b18291
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-09-24 17:37:46 +1200

    created modal, setup the initial api fetch

---
+++ packages/navigate-theme/src/hooks/useFetchHook.js
@@ -0,0 +1,17 @@
+import { useEffect, useState, useRef } from "react"
+
+export const useFetchHook = slug => {
+  // setting inital state
+  const [state, setState] = useState({ data: null, loading: true })
+
+  // loading our data
+  useEffect(() => {
+    fetch(`https://mtiapi.ntbeta.com/v1/prices/${slug}`)
+      .then(x => x.json())
+      .then(y => {
+        setState({ data: y, loading: false })
+      })
+  }, [slug, setState])
+
+  return state
+}
