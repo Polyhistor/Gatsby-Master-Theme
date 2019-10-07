a8ac37fb807894e6a328237f37309a9c2a33b32a
Lucas Santos <lucas@navigatetravel.com>
2019-10-07 14:18:52 +1300

    test deploy

--- packages/wildkiwi/gatsby-config.js
+++ packages/wildkiwi/gatsby-config.js
@@ -1,7 +1,10 @@
 // getting our environment variables
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+
+if (process.env.NODE_ENV && process.env.NODE_ENV === "development") {
+  require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+  })
+}
 
 module.exports = {
   siteMetadata: {
