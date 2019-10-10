51110340ff0c1ba75f9044a64faed0a4aab36887
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-10-10 16:51:57 +1300

    removed console logs

--- packages/navigate-theme/src/components/destinations/detailsForm.js
+++ packages/navigate-theme/src/components/destinations/detailsForm.js
@@ -90,10 +90,10 @@ const DetailsForm = ({ state, imgSlug, title }) => {
                   await submitEnquiryRequest(finalAPI)
                   setSuccess(true)
                 } catch (error) {
-                  /*console.log(
+                  console.log(
                     error,
                     "something seems to be wrong with this request"
-                  )*/
+                  )
                 }
               }}
             >
