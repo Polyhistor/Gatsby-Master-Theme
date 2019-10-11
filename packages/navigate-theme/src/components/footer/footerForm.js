66f237cdcbd664bf5d577a1a0de3bd921393f554
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-10-11 15:12:20 +1300

    added new state logic for contact for mand success message

--- packages/navigate-theme/src/components/footer/footerForm.js
+++ packages/navigate-theme/src/components/footer/footerForm.js
@@ -46,111 +46,115 @@ const FooterForm = () => {
           Feel free to drop us a note with any questions
         </p>
         <div className="footer__form-fields">
-          <Formik
-            initialValues={{
-              name: "",
-              email: "",
-              message: "",
-              consent: false,
-            }}
-            validationSchema={validationSchema}
-            onSubmit={async values => {
-              finalAPI = { ...values, ...partialData }
-              try {
-                const response = await submitContactRequest(
-                  JSON.stringify(finalAPI)
-                )
-
-                setSuccess({
-                  success: true,
-                  message: JSON.stringify(response.data.data.message),
-                })
-              } catch (error) {
-                //TODO: catch error
-                console.log(
-                  error,
-                  "Respond could not be fetched, please contact adminstrator or try again"
-                )
-              }
-            }}
-          >
-            {({
-              initialValues,
-              errors,
-              touched,
-              handleChange,
-              setFieldValue,
-              resetForm,
-            }) => (
-              <Form className="footer__contact-form">
-                <div>
-                  <Field
-                    type="text"
-                    name="name"
-                    placeholder="Name"
-                    className="footer__form-input foooter__form-input-name"
-                  />
-                  <Error touched={touched.name} message={errors.name} />
-                </div>
-                <div>
-                  <Field
-                    type="text"
-                    name="email"
-                    placeholder="Email Address *"
-                    className={
-                      errors.email
-                        ? "booking-form__fields booking-form__fields--error"
-                        : "booking-form__fields"
-                    }
-                  ></Field>
-                  <Error touched={touched.email} message={errors.email} />
-                </div>
-                <div className="booking-details__fields-container">
-                  <Field
-                    type="textarea"
-                    name="message"
-                    placeholder="Comments"
-                    className="footer__form-input--big"
-                  ></Field>
-                  <Error touched={touched.comments} message={errors.comments} />
-                </div>
-                <div className="footer__form-consent-box">
-                  <Field
-                    id="consent"
-                    name="consent"
-                    type="checkbox"
-                    className="footer__form-radio"
-                    required
-                  ></Field>
-                  <label
-                    className="footer__form-consent u-color-gray"
-                    htmlFor="consent"
-                  >
-                    I accept the&thinsp;
-                    <a
-                      className="link"
-                      href={`${process.env.GATSBY_SITE_URL}/terms-conditions`}
-                      target="_blank"
-                    >
-                      terms and conditions
-                    </a>
-                  </label>
-                </div>
-                <button
-                  id={TAG_MANAGER_TRACKER.CONTACT_FOOTER_BUTTON}
-                  className="btn btn--green-footer"
-                  type="submit"
-                >
-                  submit
-                </button>
-              </Form>
-            )}
-          </Formik>
           {success ? (
-            <h3 className="u-padding-top-medium filtered-tour__description-price success-message">
+            <h3 className="u-padding-top-medium filtered-tour__description-price">
               {message}
             </h3>
-          ) : null}
+          ) : (
+            <Formik
+              initialValues={{
+                name: "",
+                email: "",
+                message: "",
+                consent: false,
+              }}
+              validationSchema={validationSchema}
+              onSubmit={async values => {
+                finalAPI = { ...values, ...partialData }
+                try {
+                  const response = await submitContactRequest(
+                    JSON.stringify(finalAPI)
+                  )
+
+                  setSuccess({
+                    success: true,
+                    message: JSON.stringify(response.data.data.message),
+                  })
+                } catch (error) {
+                  //TODO: catch error
+                  console.log(
+                    error,
+                    "Respond could not be fetched, please contact adminstrator or try again"
+                  )
+                }
+              }}
+            >
+              {({
+                initialValues,
+                errors,
+                touched,
+                handleChange,
+                setFieldValue,
+                resetForm,
+              }) => (
+                <Form className="footer__contact-form">
+                  <div>
+                    <Field
+                      type="text"
+                      name="name"
+                      placeholder="Name"
+                      className="footer__form-input foooter__form-input-name"
+                    />
+                    <Error touched={touched.name} message={errors.name} />
+                  </div>
+                  <div>
+                    <Field
+                      type="text"
+                      name="email"
+                      placeholder="Email Address *"
+                      className={
+                        errors.email
+                          ? "booking-form__fields booking-form__fields--error"
+                          : "booking-form__fields"
+                      }
+                    ></Field>
+                    <Error touched={touched.email} message={errors.email} />
+                  </div>
+                  <div className="booking-details__fields-container">
+                    <Field
+                      type="textarea"
+                      name="message"
+                      placeholder="Comments"
+                      className="footer__form-input--big"
+                    ></Field>
+                    <Error
+                      touched={touched.comments}
+                      message={errors.comments}
+                    />
+                  </div>
+                  <div className="footer__form-consent-box">
+                    <Field
+                      id="consent"
+                      name="consent"
+                      type="checkbox"
+                      className="footer__form-radio"
+                      required
+                    ></Field>
+                    <label
+                      className="footer__form-consent u-color-gray"
+                      htmlFor="consent"
+                    >
+                      I accept the&thinsp;
+                      <a
+                        className="link"
+                        href={`${process.env.GATSBY_SITE_URL}/terms-conditions`}
+                        target="_blank"
+                      >
+                        terms and conditions
+                      </a>
+                    </label>
+                  </div>
+                  <button
+                    id={TAG_MANAGER_TRACKER.CONTACT_FOOTER_BUTTON}
+                    className="btn btn--green-footer"
+                    type="submit"
+                  >
+                    submit
+                  </button>
+                </Form>
+              )}
+            </Formik>
+          )}
         </div>
       </div>
     </>
