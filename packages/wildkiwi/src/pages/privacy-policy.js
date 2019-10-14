195714941596f950767fb99f103aa740e4370f4c
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-10-14 13:39:22 +1300

    fixed terms and conditions styling except the table

--- packages/wildkiwi/src/pages/privacy-policy.js
+++ packages/wildkiwi/src/pages/privacy-policy.js
@@ -1,5 +1,14 @@
 import React from "react"
-
+import {
+  BLOCKS,
+  INLINES,
+  HEADING_1,
+  HEADING_2,
+  OL_LIST,
+  UL_LIST,
+  CODE,
+  BOLD,
+} from "@contentful/rich-text-types"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 import {
@@ -9,13 +18,24 @@ import {
 } from "@nt-websites/navigate-theme"
 
 const Privacy = ({ data }) => {
+  const options = {
+    renderNode: {
+      [BLOCKS.PARAGRAPH]: (node, children) => (
+        <p className="activity__paragraph">{children}</p>
+      ),
+    },
+  }
+  // calling our hook
   const privacyData = usePrivacyQuery()
   return (
     <Layout2>
       {renderSeo(data)}
       <div className="section-tc">
         <article className="tour-banner__description-details u-margin-top-huge">
-          {documentToReactComponents(privacyData[0].node.description.json)}
+          {documentToReactComponents(
+            privacyData[0].node.description.json,
+            options
+          )}
         </article>
       </div>
     </Layout2>
