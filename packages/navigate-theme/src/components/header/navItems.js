1044797a794cb61728cd71f47a97182ff4c8d95f
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-10-15 11:47:39 +1300

    working the menu

--- packages/navigate-theme/src/components/header/navItems.js
+++ packages/navigate-theme/src/components/header/navItems.js
@@ -13,7 +13,7 @@ const NavItems = () => {
   const renderNavItems = () => {
     return menuLabels.map(({ label, link, sub }, idx) => {
       return (
-        // bear in mind that only the fragments with explictly written React.fragment can be keyed!, in here we added idx+5, to keye our fragments
+        // bear in mind that only the fragments with explictly written React.fragment can be keyed!, in here we added idx+5, to keye our fragments [interesting]
         <React.Fragment key={idx + 5}>
           <li key={idx} className="navigation__item">
             <Link className="navigation__link" to={link}>
