98758f0cd8db76c04a51c8048807c868f6645bd5
Lucas Santos <lucas@navigatetravel.com>
2019-10-14 15:23:26 +1300

    google tag manager

--- packages/navigate-theme/src/config/tag-manager.js
+++ packages/navigate-theme/src/config/tag-manager.js
@@ -1,8 +1,8 @@
 export const TAG_MANAGER_TRACKER = {
-  CONTACT_FOOTER_BUTTON: "contact-footer",
-  CONTACT_PAGE_BUTTON: "contact-page",
-  POPUP_LAUNCH_BUTTON: "book-start",
-  POPUP_SUBMIT_BUTTON: "book-submit",
-  IN_PAGE_SUBMIT_BUTTON: "book-submit",
-  IN_PAGE_CHECK_AVAILABILITY: "book-start",
+  CONTACT_FOOTER_BUTTON: "contact-submit-button",
+  CONTACT_PAGE_BUTTON: "contact-submit-button",
+  POPUP_LAUNCH_BUTTON: "book-start-button",
+  POPUP_SUBMIT_BUTTON: "book-submit-button",
+  IN_PAGE_SUBMIT_BUTTON: "book-submit-button",
+  IN_PAGE_CHECK_AVAILABILITY: "book-start-button",
 }
