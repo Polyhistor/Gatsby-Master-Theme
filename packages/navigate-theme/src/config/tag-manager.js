71d69a9fb75be88b886c17f79516ef65c7c5e5b1
Lucas Santos <lucas@navigatetravel.com>
2019-10-14 15:48:18 +1300

    rename tag ids

--- packages/navigate-theme/src/config/tag-manager.js
+++ packages/navigate-theme/src/config/tag-manager.js
@@ -1,6 +1,6 @@
 export const TAG_MANAGER_TRACKER = {
-  CONTACT_FOOTER_BUTTON: "contact-submit-button",
-  CONTACT_PAGE_BUTTON: "contact-submit-button",
+  CONTACT_FOOTER_BUTTON: "contact-submit-button-footer",
+  CONTACT_PAGE_BUTTON: "contact-submit-button-page",
   POPUP_LAUNCH_BUTTON: "book-start-button-popup",
   POPUP_SUBMIT_BUTTON: "book-submit-button-popup",
   IN_PAGE_SUBMIT_BUTTON: "book-submit-button-page",
