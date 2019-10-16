a9833cc05b46e8463d9aa48e3c12c56f69316c98
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-10-16 13:31:21 +1300

    fixing the buggy menu items

--- packages/navigate-theme/src/components/header/navItems.js
+++ packages/navigate-theme/src/components/header/navItems.js
@@ -11,27 +11,33 @@ const NavItems = () => {
 
   // iterating through link items
   const renderNavItems = () => {
-    return menuLabels.map(({ label, link, sub }, idx) => {
+    return menuLabels.map(({ label, link, sub, external }, idx) => {
       return (
-        // bear in mind that only the fragments with explictly written React.fragment can be keyed!, in here we added idx+5, to keye our fragments [interesting]
+        // bear in mind that only the fragments with explictly written React.fragment can be keyed!, in here we added idx+5, to keye our fragments
         <React.Fragment key={idx + 5}>
           <li key={idx} className="navigation__item">
-            <Link className="navigation__link" to={link}>
-              {label}
-            </Link>
-            {/* if there are subs available, we show them, otherwise, we don't render extra unncessary codes */}
-            {sub !== null ? (
-              <div key={label} className="navigation__dropdown">
-                {sub.map(({ label, link }) => {
-                  return (
-                    <li key={label} className="navigation__dropdown-item">
-                      <Link to={link}>{label}</Link>
-                    </li>
-                  )
-                })}
-              </div>
-            ) : null}
+            {external ? (
+              <a className="navigation__link" href={link} target="_blank">
+                {label}
+              </a>
+            ) : (
+              <Link className="navigation__link" to={link}>
+                {label}
+              </Link>
+            )}
           </li>
+          {/* if there are subs available, we show them, otherwise, we don't render extra unncessary codes */}
+          {sub !== null ? (
+            <div key={label} className="navigation__dropdown">
+              {sub.map(({ label, link }) => {
+                return (
+                  <li key={label} className="navigation__dropdown-item">
+                    <Link to={link}>{label}</Link>
+                  </li>
+                )
+              })}
+            </div>
+          ) : null}
         </React.Fragment>
       )
     })
