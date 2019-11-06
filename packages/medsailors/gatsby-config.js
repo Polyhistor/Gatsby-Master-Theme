355c8b89de9936953a0a6fda6f95ac66d9321b18
Lucas Santos <lucas@navigatetravel.com>
2019-11-07 09:09:11 +1300

    test netlify cache

--- packages/medsailors/gatsby-config.js
+++ packages/medsailors/gatsby-config.js
@@ -30,6 +30,7 @@ module.exports = {
   plugins: [
     {
       resolve: "gatsby-plugin-netlify-cache",
+      cachePublic: true,
     },
     {
       //TODO: read more about it: we are already using this plugin on shared theme, maybe we don't need to declare again or it might have some negative side effect.
