c4ad628fba94aad97c058e489104e96b718d76ed
Lucas Santos <lucas@navigatetravel.com>
2019-11-13 17:07:15 +1300

    insta images workaround for yg build

--- packages/navigate-theme/src/components/layout/layout2.js
+++ packages/navigate-theme/src/components/layout/layout2.js
@@ -14,10 +14,21 @@ import footerContext from "../../contexts/footerContext"
 //loading fonts
 import "../../styles/fonts/_fonts.css"
 import "../../styles/main.scss"
-
+import useImageQuery from "../../queries/imageQuery"
 const Layout = ({ children, Insta }) => {
   Layout.propTypes = {
     children: PropTypes.node.isRequired,
+  }
+
+  const imageQuery = useImageQuery()
+  const insta = Insta || {
+    photos: [
+      { imageOne: imageQuery.instaOneMS.childImageSharp.fluid },
+      { imageTwo: imageQuery.instaTwoMS.childImageSharp.fluid },
+      { imageThree: imageQuery.instaThreeMS.childImageSharp.fluid },
+      { imageFour: imageQuery.instaFourMS.childImageSharp.fluid },
+    ],
+    URL: "https://www.instagram.com/explore/tags/medsailors/?hl=en",
   }
 
   return (
@@ -27,7 +38,7 @@ const Layout = ({ children, Insta }) => {
         <NavigationMobile />
       </logoContext.LogoProvider>
       <main>{children}</main>
-      <footerContext.Provider value={Insta}>
+      <footerContext.Provider value={insta}>
         <Footer />
       </footerContext.Provider>
     </>
