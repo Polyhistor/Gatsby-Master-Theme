71fcdad48c78727c27a5c021945b4fbce4ad9ac0
Lucas Santos <lucas@navigatetravel.com>
2019-12-09 22:54:26 -0300

    yg theme

---
+++ packages/yachtgetaways/src/queries/fragments.js
@@ -0,0 +1,76 @@
+export const YachtFragment = graphql`
+  fragment Yacht on ContentfulOurYachts {
+    title
+    subtitle
+    order
+    description {
+      description
+    }
+    yachtMap {
+      localFile {
+        publicURL
+      }
+    }
+    yachtMap6X4 {
+        localFile{
+          publicURL
+        }
+      }
+    keyFeatures
+    images {
+      localFile {
+        childImageSharp {
+          fluid(maxWidth: 2000, quality: 90) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  }
+`
+
+export const WorkingForUsFragment = graphql`
+  fragment WorkingForUs on ContentfulWorkingForUs {
+    title1
+    description1 {
+      description1
+    }
+    description1SecondParagraph {
+      description1SecondParagraph
+    }
+    links
+    urLs
+    title2
+    description2 {
+      description2
+    }
+    title3
+    description3 {
+      description3
+    }
+    title4
+    description4 {
+      description4
+    }
+    links2
+    urLs2
+    image1 {
+      localFile {
+        childImageSharp {
+          fluid(maxWidth: 1000, quality: 90) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+    image2 {
+      localFile {
+        childImageSharp {
+          fluid(maxWidth: 1000, quality: 90) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  }
+`
