253f27bfbea1168681442e8b19b4d444f64b8bf7
Pouya Ataei <pouya.ataei.7@gmail.com>
2020-01-13 16:36:46 +1300

    [YG] - fixed bottom banner link

--- packages/yachtgetaways/src/pages/faq.js
+++ packages/yachtgetaways/src/pages/faq.js
@@ -60,7 +60,7 @@ const FAQ = ({ data }) => {
         subHeaderFirst="Book your own"
         subHeaderSecond="private sailing trip"
         buttonText={howItWorksBannerText}
-        link="/how-it-works"
+        link="/private-yacht-charters"
       />
       <Reviews />
       <Trips
