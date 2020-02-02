4206402dceae5ff8f791f63ddc5c76175a07e8fb
Pouya Ataei <pouya.ataei.7@gmail.com>
2020-02-02 15:47:16 +1300

    [ALL] - refarctored mobile menu

--- packages/navigate-theme/src/components/mobile/mobileNavItems.js
+++ packages/navigate-theme/src/components/mobile/mobileNavItems.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 
 import NavButton from "../header/navButton"
 import useMenuLabelQuery from "../../queries/themeMenuLabelConfigQuery"
+import resolveVariationClass from "../../helpers/theme-variation-style"
 
 const MobileNavItems = () => {
   const menuLabels = useMenuLabelQuery()
@@ -13,7 +14,14 @@ const MobileNavItems = () => {
       return (
         // bear in mind that only the fragments with explictly written React.fragment can be keyed!, in here we added idx+5, to keye our fragments
         <React.Fragment key={idx + 5}>
-          <li key={idx} className="navigation__item">
+          <li
+            key={idx}
+            className={
+              idx === 0
+                ? `${resolveVariationClass("navigation__link")}`
+                : "navigation__link"
+            }
+          >
             {external ? (
               <a className="navigation__link" href={link} target="_blank">
                 {label}
